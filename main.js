0951df22a6da8a4f5ee56b9ab44fff4bcca4c7e5
Felipe <felipe.g.pavanelli.26@dartmouth.edu>
2023-04-20 17:47:41 -0400

    css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+function scrollToImage(){
+    const funImg = document.getElementById("breading");
+    funImg.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+
+// var inputVariable = "";
+// function in
+// inputVariable = 
